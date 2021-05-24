b1c23bdee62f22faa1e71f340ee59f1e5f5aa9e3
dunlopWill <62902332+will.dunlop@users.noreply.github.com>
2021-05-24 08:49:54 +0100

    updated 18

--- chapter_1_a_tutorial_introduction/exercise_1-18.c
+++ chapter_1_a_tutorial_introduction/exercise_1-18.c
@@ -1,57 +1,57 @@
-/* Exercise 1-18.c. Write a program to remove trailing blanks and tabs from
-each line of input, and to detlete entirely blank lines */
-
-/*
-1. Get all input and copy into an array
-2. While copying, if character is a blank (' ') and subsequent is newline ('\n')
-then overwrite the blank with the '\n'.
-3. If character is a tab '\t', then do not copy into array.
-4. If character is '\n' and all subsequent characters are ' ' or '\t' until
-next '\n' then do not copy into array
-*/
+/* Exercise 1-18. Write a program to remove trailing blanks and tabs
+from each line of input, and delete entirely blank lines. */
 
 #include <stdio.h>
 #define MAX_INPUT 1000 /* maxium input of characters size */
-#define TARGET_LENGTH 80 /* print all input lines longer than this number of characters */
 
 int input_length(char all_input[]); /* Return length of input including newline characters */
-void print_line(char all_input[], int start_positition, int finish_position);
-int establish_line_length(char all_input[], int input_length, int target_length, int start_positition);
+void print_input(char all_input[]);
+int end_of_line(char all_input[], int start_positition);
+int line_wo_blanks(char all_input[], int start_positition, int end);
 
-/* print longest input line */
 int main(void)
 {
-    int c, length, start_positition, finish_position;
-    int max; /* maximum length seen so far */
-    char all_input[MAX_INPUT]; /* current input*/
+    int c, i, j,length, count, start_position, finish_position, amount_to_copy, leave_loop;
+    char all_input[MAX_INPUT], cleaned_input[MAX_INPUT]; /* current input*/
 
-    int i = 0;
+    i = j = leave_loop = 0;
+    count = 1;
 
+    /* Get input */
     printf("Please input up to %d characters and then click Ctrl+D\n", (MAX_INPUT - 1));
     while ((c = getchar()) != EOF)
     {
       all_input[i] = c;
       i++;
+      /* Handle too much input */
+      if (i >= MAX_INPUT)
+        printf("Max number of characters exceeded.\n");
     }
 
-    if (i >= MAX_INPUT)
-      printf("Max number of characters exceeded.\n");
 
     length = input_length(all_input);
 
     if (length > 0)
     {
-      start_positition = 0;
-      while (start_positition <= length)
+      start_position = 0;
+
+      while (start_position < length)
       {
-      finish_position = establish_line_length(all_input, length, TARGET_LENGTH, start_positition);
+      finish_position = line_wo_blanks(all_input, start_position, end_of_line(all_input, start_position));
 
-      if ((finish_position - start_positition) >= TARGET_LENGTH)
-        print_line(all_input, start_positition, finish_position);
+      for (i = start_position; i <= finish_position; i++)
+        printf("%c", all_input[i]);
+        count++;
+      printf("\n");
+      count++;
 
-      start_positition = finish_position + 2;
+      start_position = end_of_line(all_input, start_position) + 1;
+      while (all_input[start_position] == '\n')
+        start_position++;
       }
     }
+
+    printf("input was originally %d\ncleaned input is %d", length, count);
 
     return 0;
 }
@@ -66,27 +66,27 @@ int input_length(char all_input[])
   return i;
 }
 
-int establish_line_length(char all_input[], int input_length, int target_length, int start_positition)
+/* Go through array and find end of line '\n' and return position */
+int end_of_line(char all_input[], int start_position)
 {
   int i;
-  int temp_line[input_length];
-  int finish_position;
-
-  for (i = start_positition; i <= input_length; i++)
+  i = start_position;
+  while (all_input[i] != '\n' && all_input[i] != '\0')
   {
-    if (all_input[i] == '\n' || all_input[i] == '\0')
-    {
-        finish_position = i - 1;
-        return finish_position;
-    }
+    i++;
   }
-  return 0;
+  return i;
 }
 
-void print_line(char all_input[], int start_positition, int finish_position)
+/* While character is a space or tab or '\n' go back 1 character and return
+   position of first non-space character. */
+int line_wo_blanks(char all_input[], int start_position, int end)
 {
   int i;
-  for (i = start_positition; i <= finish_position; i++)
-    printf("%c", all_input[i]);
-  printf("\n");
+  i = end;
+  while (all_input[i] == ' ' || all_input[i] == '\t' || all_input[i] == '\n')
+  {
+    i--;
+  }
+  return i;
 }
